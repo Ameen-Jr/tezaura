2c6e41ef6f095be66572fee20d9ef8202bd63eeb
Ameen_Jr <ameenjawhares@gmail.com>
2026-04-16 09:43:09 +0530

    SafeLottie CDN fixed to run offline and also unwanted files deleted in the previous commit

--- frontend/src/components/SafeLottie.jsx
+++ frontend/src/components/SafeLottie.jsx
@@ -15,7 +15,7 @@ const SafeLottie = ({ animationData }) => {
       </head>
       <body>
         <div id="lottie"></div>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/lottie-web/5.12.2/lottie.min.js"></script>
+        <script src="/lottie.min.js"></script>
         <script>
           try {
             const animData = ${jsonString}; 
