76b86041d616142bb01bb69306cef656523d9c75
Ameen_Jr <ameenjawhares@gmail.com>
2026-04-16 09:32:59 +0530

    CORS Origin header updated

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ async def startup_auto_backup():
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "tauri://localhost", "http://tauri.localhost"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
